bca5f3084f55152d32ee100f2d959c040de47700
1MasonMuch <masonm126@nycstudents.net>
2025-12-04 09:26:11 -0500

    2

--- project.py
+++ project.py
@@ -1,26 +1,25 @@
 from tkinter import *
 import requests
-
-
 root = Tk()
 
-def myclick():
-    mylabel3 = Label(root, text = f"Hello {e.get()} ")
-    mylabel3.grid(row = 3, column = 2)
+def getPoke(poke):
+    response = requests.get(f"https://api.frankfurter.dev/v1/latest{poke.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
 
-e = Entry(root, width = 20, borderwidth = 3)
-e.grid(row = 2, column = 1)
-e.insert(0, "Enter Your name:")
+pokemon = getPoke("Bulbasaur")
+print(pokemon)
 
-myLabel1 = Label(root, text = "Helllo World!")
-myLabel1.grid(row = 0, column = 0)
 
-myLabel2 = Label(root, text = "My name is Mason")
-myLabel2.grid(row = 1, column = 1)
 
-mybutton = Button(root, text="Enter your name!",
-                  padx=10, pady=10, command=myclick,
-                  fg="white", bg="black")
-mybutton.grid(row = 2 , column = 2)
 
 root.mainloop()
