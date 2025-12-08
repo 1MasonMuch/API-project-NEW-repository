3909242870c0a09e24abde28700399d518d3a779
1MasonMuch <masonm126@nycstudents.net>
2025-12-08 09:26:13 -0500

    6

--- project.py
+++ project.py
@@ -3,18 +3,23 @@ import requests
 root = Tk()
 api = requests.get(f"https://api.frankfurter.dev/v1/latest?base=USD")
 data = api.json()
-
-#def convert():
- #   for i in api:
-  #      if choose.get == i[api]:
+rates = data["rates"]
+def convert():
+  
+  for i in api:
+    if choose_currency.get == i[data]:
+      amt = amt * i[rates]
         
 
 choose_currency = Entry(root, width = 10, borderwidth = 2)
 currency_input = Label(root, text = "Input a currency you would like to convert USD to:", font = "Arial")
+submit = Button(root, text="Submit", padx= 5, pady= 5)
+amt = Entry(root, width = 10, borderwidth= 2)
 
+
+currency_input.grid(row = 0, column = 0)
 choose_currency.grid(row = 1, column = 0)
-choose_currency.grid(row = 0, column = 0)
-
+submit.grid(row = 3, column = 0)
 
 
 root.mainloop()
